8616af854bd7d1629c7e1213c90930fcb937d33c
mustaphaas <63932124+mustaphaas@users.noreply.github.com>
2026-09-06 14:23:49 +0100

    Revert sequential upload queue redesign

--- client/App.tsx
+++ client/App.tsx
@@ -27,7 +27,6 @@ import ConsultantCoverageMapEnhancer from "./components/ConsultantCoverageMapEnh
 import ConsultantProjectLgaDrilldown from "./components/ConsultantProjectLgaDrilldown";
 import FieldOfficerOfflineSyncTab from "./components/FieldOfficerOfflineSyncTab";
 import FieldOfficerPendingUploads from "./components/FieldOfficerPendingUploads";
-import FieldOfficerSequentialUploadQueue from "./components/FieldOfficerSequentialUploadQueue";
 import { AuthProvider, RequireRole, useAuth } from "./lib/auth";
 import { InspectionWorkflowProvider } from "./lib/inspection-workflow";
 
@@ -107,7 +106,6 @@ const App = () => (
             <ConsultantProjectLgaDrilldown />
             <FieldOfficerOfflineSyncTab />
             <FieldOfficerPendingUploads />
-            <FieldOfficerSequentialUploadQueue />
             <VeritasFooterGate />
             <VeritasGate />
           </BrowserRouter>
