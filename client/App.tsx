de02ac25933cf8571835c4a22aaf346c271c6e41
mustaphaas <63932124+mustaphaas@users.noreply.github.com>
2026-09-06 03:34:37 +0100

    Wire field officer GPS and navigation enhancer

--- client/App.tsx
+++ client/App.tsx
@@ -24,6 +24,7 @@ import VeritasFooter from "./components/VeritasFooter";
 import ReaProjectMapHost from "./components/ReaProjectMapProgramme";
 import ProjectMapFullscreenControl from "./components/ProjectMapFullscreenControl";
 import ConsultantCoverageMapEnhancer from "./components/ConsultantCoverageMapEnhancer";
+import FieldOfficerLocationEnhancer from "./components/FieldOfficerLocationEnhancer";
 import { AuthProvider, RequireRole, useAuth } from "./lib/auth";
 import { InspectionWorkflowProvider } from "./lib/inspection-workflow";
 
@@ -100,6 +101,7 @@ const App = () => (
             </Routes>
             <ProjectMapGate />
             <ConsultantCoverageMapEnhancer />
+            <FieldOfficerLocationEnhancer />
             <VeritasFooterGate />
             <VeritasGate />
           </BrowserRouter>
