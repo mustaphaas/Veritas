3ec403ee3c2b2e2d7cdc1cef592681d35bd7da61
mustaphaas <63932124+mustaphaas@users.noreply.github.com>
2026-09-06 03:42:36 +0100

    Restore original field officer GPS layout

--- client/App.tsx
+++ client/App.tsx
@@ -24,7 +24,6 @@ import VeritasFooter from "./components/VeritasFooter";
 import ReaProjectMapHost from "./components/ReaProjectMapProgramme";
 import ProjectMapFullscreenControl from "./components/ProjectMapFullscreenControl";
 import ConsultantCoverageMapEnhancer from "./components/ConsultantCoverageMapEnhancer";
-import FieldOfficerLocationEnhancer from "./components/FieldOfficerLocationEnhancer";
 import { AuthProvider, RequireRole, useAuth } from "./lib/auth";
 import { InspectionWorkflowProvider } from "./lib/inspection-workflow";
 
@@ -101,7 +100,6 @@ const App = () => (
             </Routes>
             <ProjectMapGate />
             <ConsultantCoverageMapEnhancer />
-            <FieldOfficerLocationEnhancer />
             <VeritasFooterGate />
             <VeritasGate />
           </BrowserRouter>
