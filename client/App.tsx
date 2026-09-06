d494f839be3173038b98ccb8909fd0a76a12c6a4
mustaphaas <63932124+mustaphaas@users.noreply.github.com>
2026-09-06 14:32:04 +0100

    Enable GPS mock on field officer demo

--- client/App.tsx
+++ client/App.tsx
@@ -27,6 +27,7 @@ import ConsultantCoverageMapEnhancer from "./components/ConsultantCoverageMapEnh
 import ConsultantProjectLgaDrilldown from "./components/ConsultantProjectLgaDrilldown";
 import FieldOfficerOfflineSyncTab from "./components/FieldOfficerOfflineSyncTab";
 import FieldOfficerPendingUploads from "./components/FieldOfficerPendingUploads";
+import FieldOfficerCoordinateMock from "./components/FieldOfficerCoordinateMock";
 import { AuthProvider, RequireRole, useAuth } from "./lib/auth";
 import { InspectionWorkflowProvider } from "./lib/inspection-workflow";
 
@@ -106,6 +107,7 @@ const App = () => (
             <ConsultantProjectLgaDrilldown />
             <FieldOfficerOfflineSyncTab />
             <FieldOfficerPendingUploads />
+            <FieldOfficerCoordinateMock />
             <VeritasFooterGate />
             <VeritasGate />
           </BrowserRouter>
