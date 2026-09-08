64deb3685f44cf594cb049d58ac85363a38a423a
mustaphaas <63932124+mustaphaas@users.noreply.github.com>
2026-09-08 02:46:33 +0100

    Mount sync media mock display

--- client/App.tsx
+++ client/App.tsx
@@ -28,6 +28,7 @@ import ConsultantProjectLgaDrilldown from "./components/ConsultantProjectLgaDril
 import ConsultantWorkflowBridge from "./components/ConsultantWorkflowBridge";
 import FieldOfficerCoordinateMock from "./components/FieldOfficerCoordinateMock";
 import FieldOfficerSyncQueueMount from "./components/FieldOfficerSyncQueueMount";
+import FieldOfficerSyncMediaMock from "./components/FieldOfficerSyncMediaMock";
 import FieldOfficerKpiDataBridge from "./components/FieldOfficerKpiDataBridge";
 import FieldOfficerGpsKmDisplay from "./components/FieldOfficerGpsKmDisplay";
 import FieldOfficerDraftsNavEnhancer from "./components/FieldOfficerDraftsNavEnhancer";
@@ -113,6 +114,7 @@ const App = () => (
             <ConsultantProjectLgaDrilldown />
             <FieldOfficerCoordinateMock />
             <FieldOfficerSyncQueueMount />
+            <FieldOfficerSyncMediaMock />
             <FieldOfficerKpiDataBridge />
             <FieldOfficerGpsKmDisplay />
             <FieldOfficerDraftsNavEnhancer />
